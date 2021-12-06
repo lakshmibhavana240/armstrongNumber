8d3402980d672df97ccd77f10b6d168522a5e81c
lakshmibhavana240 <92907244+lakshmibhavana240@users.noreply.github.com>
2021-12-06 09:54:12 -0500

    Add files via upload

---
+++ AdditionOf2Numbers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class AdditionOf2Numbers {
+    public static void main(String[] args) {
+      int num1 = 20, num2 = 15, sum;
+      sum = num1 + num2;
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
